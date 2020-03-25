33d49cb8392f771565ed52e02484d7707113af36
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-03-25 15:47:26 +0100

    Update Very_important.R

--- Very_important.R
+++ Very_important.R
@@ -3,3 +3,6 @@
 apply(is.na(Janvier_DATA),2,sum)
 #RandomForest 
 DATA.train <- DATA.train %>% mutate_if(is.character, as.factor)
+
+is.finite(c(1, NA, Inf, NaN, -Inf))
+#[1]  TRUE FALSE FALSE FALSE FALSE
