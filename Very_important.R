a8bd0dd645126655fc1bbef08b267f52369a2782
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-03-25 17:30:37 +0100

    Update Very_important.R

--- Very_important.R
+++ Very_important.R
@@ -6,3 +6,10 @@ DATA.train <- DATA.train %>% mutate_if(is.character, as.factor)
 
 is.finite(c(1, NA, Inf, NaN, -Inf))
 #[1]  TRUE FALSE FALSE FALSE FALSE
+
+#color top
+library(RColorBrewer)
+n <- 60
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+pie(rep(1,n), col=sample(col_vector, n))
