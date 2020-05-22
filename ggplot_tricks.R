bba41ee7aa02021cdee8ab491c3a7db192433e4b
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-05-22 11:13:37 +0000

    Update ggplot_tricks.R

--- ggplot_tricks.R
+++ ggplot_tricks.R
@@ -4,4 +4,8 @@
   ggplot(aes(x = V1,y = V2))+
   geom_point(col ="red")+
   geom_point(col="green",aes(y=V3))+
-  scale_x_continuous(minor_breaks = seq(0.1,1,0.01)) #<--------------
+  scale_x_continuous(minor_breaks = seq(0.1,1,0.01),
+                     breaks =  seq(0.1,1,0.1),
+                     labels = seq(0.1,1,0.1) )+
+  # theme_bw() +
+  theme(axis.text.x=element_text(size=rel(1.5), angle=90))
