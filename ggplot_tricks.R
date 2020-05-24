2fd7986ed68e7fd8a64cbdca63fe06ec2a015263
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-05-24 17:13:58 +0100

    Update ggplot_tricks.R

--- ggplot_tricks.R
+++ ggplot_tricks.R
@@ -1,14 +1,14 @@
-  #How to control number of minor grid lines in ggplot2?
-  grid_df %>% 
-  as.data.frame() %>% 
-  ggplot(aes(x = V1,y = V2))+
-  geom_point(col ="red")+
-  geom_point(col="green",aes(y=V3))+
-  scale_x_continuous(minor_breaks = seq(0.1,1,0.01),
-                     breaks =  seq(0.1,1,0.1),
-                     labels = seq(0.1,1,0.1) )+
-  # theme_bw() +
-  theme(axis.text.x=element_text(size=rel(1.5), angle=90))
+#How to control number of minor grid lines in ggplot2?
+grid_df %>% 
+as.data.frame() %>% 
+ggplot(aes(x = V1,y = V2))+
+geom_point(col ="red")+
+geom_point(col="green",aes(y=V3))+
+scale_x_continuous(minor_breaks = seq(0.1,1,0.01),
+	     breaks =  seq(0.1,1,0.1),
+	     labels = seq(0.1,1,0.1) )+
+# theme_bw() +
+theme(axis.text.x=element_text(size=rel(1.5), angle=90))
 
 # Create graph for each column
 graph <- lapply(names(factor),
@@ -25,6 +25,17 @@ ggcorr(Train[, -ncol(Train)],
        hjust = 0.9, # <---- very important
        label_alpha = TRUE)
 
+# Plot the ROC curves
+library(ROCR)
+m <- length(preds_list)
+actuals_list <- rep(list(actual_test), m)
+pred <- ROCR::prediction(preds_list, actuals_list)
+rocs <- performance(pred, "tpr", "fpr")
+plot(rocs, col = as.list(1:m), main = "Test Set ROC Curves")
+legend(x = "bottomright", 
+       legend = c("Decision Tree", "Bagged Trees", "Random Forest", "GBM","XGB","ANN","KNN","glm"),
+       fill = 1:m,
+       cex = 0.75)
 
 
 
