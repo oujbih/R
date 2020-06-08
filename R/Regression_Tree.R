292d4f2480d5e0df6a078bd99685a6eacca9e17a
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-06-08 03:02:32 +0100

    Create Regression_Tree.R

---
+++ R/Regression_Tree.R
@@ -0,0 +1,41 @@
+
+hyper_grid <- expand.grid(
+  minsplit = seq(5, 20, 1),
+  maxdepth = seq(8, 15, 1)
+)
+
+models <- list()
+
+for (i in 1:nrow(hyper_grid)) {
+
+  # get minsplit, maxdepth values at row i
+  minsplit <- hyper_grid$minsplit[i]
+  maxdepth <- hyper_grid$maxdepth[i]
+
+  # train a model and store in the list
+  models[[i]] <- rpart(formula = c, 
+                       data = Train_df_cl, 
+                       method = "anova",
+                       control = list(minsplit = minsplit, maxdepth = maxdepth))
+
+}
+
+# function to get optimal cp
+get_cp <- function(x) {
+  min    <- which.min(x$cptable[, "xerror"])
+  cp <- x$cptable[min, "CP"] 
+}
+
+# function to get minimum error
+get_min_error <- function(x) {
+  min    <- which.min(x$cptable[, "xerror"])
+  xerror <- x$cptable[min, "xerror"] 
+}
+
+hyper_grid %>%
+  mutate(
+    cp    = purrr::map_dbl(models, get_cp),
+    error = purrr::map_dbl(models, get_min_error)
+    ) %>%
+  arrange(error) %>%
+  top_n(-5, wt = error)
